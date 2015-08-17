6ecf3c37ca04bc41ea82f6902402cd714b19269e
Jayadev Peddibhotla <jayadev.peddibhotla@kony.com>
2015-08-17 19:28:09 +0700

    commit changes

--- src/DateTime.js
+++ src/DateTime.js
@@ -1,5 +1,6 @@
 // Given a date in string format, return the day it corresponds to.
 exports.GetDay = function(dateString){
-
+var milliSeconds = Date.parse(dateString);
+return isNaN(milliSeconds)? milliSeconds:new Date(milliSeconds).getDay();
 }
 
