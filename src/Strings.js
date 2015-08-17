d73e9e474b59fe290fefbdb820bba17c504aa086
Jayadev Peddibhotla <jayadev.peddibhotla@kony.com>
2015-08-17 21:39:40 +0700

    commit changes

--- src/Strings.js
+++ src/Strings.js
@@ -1,5 +1,5 @@
 exports.ReverseString = function(str){
-
+    return str.split("").reverse().join("");
 }
 
 // Given a string with lname,fname - return an array with the first names.
@@ -7,16 +7,26 @@ exports.ReverseString = function(str){
 // an array ["Bill","Steve","Larry"]
 
 exports.GetFirstNames = function(str){
+    var firstNames = null;
+    if(str != null) {
+        var splitStringBySemiColon = str.split(";");
+        firstNames=new Array();
+        for (var i in splitStringBySemiColon) {
+            if (splitStringBySemiColon[i] != "") {
+                firstNames.push(splitStringBySemiColon[i].split(",")[1]);
+            }
+        }
+    }
+    return firstNames;
 }
 
 
 exports.ReverseArrayOfStrings = function(arrayOfStrings){
-
+return arrayOfStrings.reverse();
 }
 
 // Given an array of sentences, create a paragraph by concatenating all the strings and adding
 // fullstop (.) after each senetences.
-exports.CreateParagraph = function(arrayOfStrings){
-
+exports.CreateParagraph = function(arrayOfStrings) {
+    return (arrayOfStrings==null)?null:((arrayOfStrings.length != 0)?arrayOfStrings.join(".") + ".":[]);
 }
-
