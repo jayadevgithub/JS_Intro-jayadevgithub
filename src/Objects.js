f8e3e98ac27031ab27ab14f83a1ecf183891e0d3
Jayadev Peddibhotla <jayadev.peddibhotla@kony.com>
2015-08-18 16:07:02 +0700

    commit changes

--- src/Objects.js
+++ src/Objects.js
@@ -1,8 +1,25 @@
 exports.GetObjectPropertyNames = function(obj) {
-
+    if(obj == null){
+        return null;
+    }
+    var propertyNameArray=new Array();
+    for(var property in obj){
+        if(obj.hasOwnProperty(property)){
+            propertyNameArray.push(property);
+        }
+    }
+    return propertyNameArray;
 }
 
 exports.GetObjectPropertyValues = function(obj) {
-
+    if(obj == null){
+        return null;
+    }
+    var propertyValueArray=new Array();
+    for(var property in obj){
+        if(obj.hasOwnProperty(property)){
+            propertyValueArray.push(obj[property]);
+        }
+    }
+    return propertyValueArray;
 }
-
