dbb30d766cedbfde107d95e6d0d54b2f00282076
Jayadev Peddibhotla <jayadev.peddibhotla@kony.com>
2015-08-18 19:59:53 +0700

    commit changes

--- src/Serialization.js
+++ src/Serialization.js
@@ -1,37 +1,45 @@
 exports.ParseNumber = function(str){
-
+    return parseInt(str);
 }
 
 exports.ConvertNumberToString = function(num){
-
+    return !isNaN(num)?num.toString():"";
 }
 
 exports.ParseDate = function(str){
-
+    return str==null?NaN:new Date(str);
 }
 
 exports.ConvertDateToString = function(dateValue){
-
+    return dateValue.toString();
 }
 
 //Parse a string containing a comma seperated sequence of numbers
 // and return an array containing the numbers. Return NaN for invalid
 // numbers in the string.
 exports.ParseStringOfNumbers = function(str){
+    var arrayOfNums = (!str=="" && typeof str === 'string')?str.split(","):[];
+    for(var strNum in arrayOfNums){
+        if(arrayOfNums[strNum]!=""){
+            arrayOfNums[strNum]=exports.ParseNumber(arrayOfNums[strNum]);
+        }
+    }
+    return arrayOfNums;
 }
 
 exports.ConvertArrayOfNumbersToString = function(obj){
-
-
+    return (obj==[]||typeof obj === 'string')?"":obj.toString();
 }
 
 
 exports.ConvertStringToObject = function(str){
-
+    var pattern = /({"|":")/;
+    return (isNaN(str)&& pattern.test(str))?JSON.parse(str):null;
 }
 
 exports.ConvertObjectToString = function(obj){
-
+    return isNaN(obj)?JSON.stringify(obj):null;
 }
 
-
+var res=exports.ConvertStringToObject("{firstname:\"Bill\",lastname:\"Gates\"}")
+console.log(res)
